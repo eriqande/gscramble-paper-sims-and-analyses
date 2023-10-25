976980e92e37235ec6d7ffa1f230d765c2425d26
Eric C. Anderson <eric.anderson@noaa.gov>
2023-10-25 11:53:21 -0600

    Finish up the permutations plot

--- R/create-gsps-for-paper.R
+++ R/create-gsps-for-paper.R
@@ -6,3 +6,13 @@ library(gscramble)
 #### Make a simple F2 GSP  ####
 F2 <- create_GSP("A", "B", F2 = TRUE)
 
+dir.create("results/figures/gsps-for-paper", recursive = TRUE, showWarnings = FALSE)
+gsp2dot(
+  F2, 
+  path = "results/figures/gsps-for-paper/F2", 
+  indiv_node_label_font_size = 36, 
+  sample_node_label_font_size = 36, 
+  edge_label_font_size = 36
+)
+
+system(" rsvg-convert -f pdf -o results/figures/gsps-for-paper/F2.pdf results/figures/gsps-for-paper/F2.svg")
