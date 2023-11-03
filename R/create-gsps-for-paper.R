5165d41f0d150de97a8492b71d7c51068b32ab6c
Eric C. Anderson <eric.anderson@noaa.gov>
2023-11-03 11:21:56 -0600

    Add a more complex GSP for the paper

--- R/create-gsps-for-paper.R
+++ R/create-gsps-for-paper.R
@@ -3,6 +3,7 @@
 library(gscramble)
 
 
+
 #### Make a simple F2 GSP  ####
 F2 <- create_GSP("A", "B", F2 = TRUE)
 
@@ -16,3 +17,20 @@ gsp2dot(
 )
 
 system(" rsvg-convert -f pdf -o results/figures/gsps-for-paper/F2.pdf results/figures/gsps-for-paper/F2.svg")
+
+
+
+
+#### Make a GSP with and F1, F2, BC1 and BC_2  ####
+AllThem <- create_GSP("A", "B", F1 = TRUE, F2 = TRUE, F1B = TRUE, F1B2 = TRUE)
+
+dir.create("results/figures/gsps-for-paper", recursive = TRUE, showWarnings = FALSE)
+gsp2dot(
+  AllThem, 
+  path = "results/figures/gsps-for-paper/AllThem", 
+  indiv_node_label_font_size = 36, 
+  sample_node_label_font_size = 36, 
+  edge_label_font_size = 36
+)
+
+system(" rsvg-convert -f pdf -o results/figures/gsps-for-paper/AllThem.pdf results/figures/gsps-for-paper/AllThem.svg")
